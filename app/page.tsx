05e4298700ec2983f337ac91a8c2b45f242599ac
Carlo Prato <pratino82@users.noreply.github.com>
2026-08-04 09:36:17 -0400

    Link Italian translation

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
           </div>
           <ol className="publication-list">
             <li data-number="02"><p><RichText html={academic.otherWriting.html} /></p></li>
-            <li data-number="01"><p>La Democrazia: Breve Saggio di Adam Przeworski <span className="item-note">[Italian translation]</span></p></li>
+            <li data-number="01"><p><a href="https://www.dropbox.com/scl/fi/04b5k0vj4up01k35a294u/Adam-S.pdf?rlkey=sh51sp9398obv63gintt4ourj&dl=0" target="_blank" rel="noopener">La Democrazia: Breve Saggio di Adam Przeworski</a> <span className="item-note">[Italian translation]</span></p></li>
           </ol>
         </section>
       </div>
